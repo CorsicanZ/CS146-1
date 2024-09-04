2df49837040c80d17e059ad7dca9f2389b4c28a6
Cor <1035711820@qq.com>
2024-09-04 22:58:59 +0800

    Finish UI and Some other geek things
    Can encouter 3 obstacles which isn't shown

--- CS146-1/Assets/Script/GameManager.cs
+++ CS146-1/Assets/Script/GameManager.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject player;
     public Text score_text;
     public Text instruction_text;
+    public Text end_text;
     private int player_health = 3;
     private float spawn_time = 0;
     private float score = 0;
@@ -19,15 +21,17 @@ public class GameManager : MonoBehaviour
     {
         spawn_time = Time.time;
 
-        ObstacleSpawner obs = FindObjectOfType<ObstacleSpawner>();
-        obs.enabled = false;
-
+        FindObjectOfType<ObstacleSpawner>().enabled = false;
         player.GetComponent<PlayerController>().enabled = false;
+        end_text.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.Escape))
+            Application.Quit();
+
         if (!game_started)
         {
             if (Input.GetKey(KeyCode.Space))
@@ -46,7 +50,13 @@ public class GameManager : MonoBehaviour
             else return;
         } 
 
-        if (game_ended) return;
+        if (game_ended)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                SceneManager.LoadScene("Menu");
+            }
+        }
 
         score = Time.time - spawn_time;
         score_text.text = score.ToString("F1");
@@ -71,6 +81,7 @@ public class GameManager : MonoBehaviour
 
         Debug.Log("Game over");
         player.GetComponent<PlayerController>().enabled = false;
+        end_text.enabled = true;
         game_ended = true;
     }
 }
